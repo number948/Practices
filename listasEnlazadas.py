c6284fdc44de2c9a9e2c15ced079444a0c621e18
a.carocavillegas <SU-CORREO-ELECTRONICO>
2021-11-16 14:02:01 -0300

    actualización

--- listasEnlazadas.py
+++ listasEnlazadas.py
@@ -1,16 +1,47 @@
+
+
+
 class Nodo:
     def __init__(self, carga = None, siguiente = None):
         self.carga = carga
         self.siguiente = siguiente
+        
 
     def __str__(self):
         return str(self.carga)
 
-    def imprimeLista(self):
-        while self:
+    def imprimeLista(nodo, self):
         
-            print(self),
-            self = self.siguiente
+        while nodo:
+            print(nodo),
+            nodo = nodo.siguiente
+       
+
+    def imprimeAlReves(self,lista):
+        if lista == None: 
+            return
+        self.imprimeAlReves(lista.siguiente)
+        print(lista)
+        
+
+    def imprimeAlRevesBonito(self,lista):
+        print("["),
+        if lista != None:
+            cabeza = lista
+            cola = lista.siguiente
+            self.imprimeAlReves(cola)
+            print(cabeza)
+        print("]"),
+
+
+
+    def eliminaSegundo(self, lista):
+        if lista == None: return
+        primero = lista
+        segundo = lista.siguiente
+        primero.siguiente = segundo.siguiente
+        segundo.siguiente = None
+        return segundo
         
 
 
@@ -19,7 +50,7 @@ nodo2 = Nodo(2)
 nodo3 = Nodo(3)
 
 #nodo1.imprimeLista(nodo1)
-#PROBANDO GIT 
+
 
 # HACIENDO QUE NODOS HAGAN REFERENCIA AL SIGUIENTE:
 #  ENLAZANDO LA LISTA DE NODOS
@@ -27,8 +58,13 @@ nodo3 = Nodo(3)
 nodo1.siguiente = nodo2
 nodo2.siguiente = nodo3
 
+#nodo1.imprimeLista(nodo1)
 
-nodo1.imprimeLista()
+
+"""eliminado = nodo1.eliminaSegundo(nodo1)
+nodo1.imprimeAlReves(eliminado)
+nodo1.imprimeLista(nodo1)"""
+nodo1.imprimeAlRevesBonito(nodo1)
 
 #print(nodo1)
 
